ac5876584adda673bec6d276d3ac079350901ca2
Kwabena Sapong <kwabenasapong@gmail.com>
2022-11-22 15:04:41 +0000

    Update the repo

--- hello_cargo/target/debug/hello_cargo.d
+++ hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/home/vagrant/rust_projects/hello_cargo/target/debug/hello_cargo: /home/vagrant/rust_projects/hello_cargo/src/main.rs
+/home/koby/Personal_dir/rust_projects/hello_cargo/target/debug/hello_cargo: /home/koby/Personal_dir/rust_projects/hello_cargo/src/main.rs
